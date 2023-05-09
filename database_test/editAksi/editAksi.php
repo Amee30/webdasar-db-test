398ec05b5e02154ee5033e9fbc0ab2df3b6fcc92
Ame <darmaputra2004@gmail.com>
2023-05-09 19:52:36 +0800

    Small Change
    Remove html element on pure php file

--- database_test/editAksi/editAksi.php
+++ database_test/editAksi/editAksi.php
@@ -1,14 +1,5 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <?php
-    include '.../koneksi.php';
+    include '../koneksi.php';
     global $koneksi;
     $getID = $_POST['id'];
     $getName = $_POST['nama'];
@@ -19,7 +10,3 @@
     mysqli_query($koneksi, "UPDATE mahasiswa SET id = '$getID', nama = '$getName', nim = '$getNim', alamat = '$getAddress' WHERE id = '$getIDTarget'");
     header("location:../dataMhs.php");
 
-?>
-
-</body>
-</html>
