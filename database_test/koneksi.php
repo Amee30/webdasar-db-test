d83567761c9e6fd1b3ce36c5fa4ec0734c0e59da
Ame <101972841+Amee30@users.noreply.github.com>
2023-05-09 14:52:23 +0800

    Update koneksi.php

--- database_test/koneksi.php
+++ database_test/koneksi.php
@@ -11,7 +11,7 @@
         $db_host = "localhost";
         $db_user = "root";
         $db_pass = "";
-        $db_name = "akademik";
+        $db_name = "";
 
         $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
